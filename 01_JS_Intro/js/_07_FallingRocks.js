730188f29b47101b80e0d02a66d1ac668984b5a4
AKJ1 <kellerr@abv.bg>
2014-07-19 01:05:29 +0300

    16 fixed

--- 01_JS_Intro/js/_07_FallingRocks.js
+++ 01_JS_Intro/js/_07_FallingRocks.js
@@ -101,3 +101,6 @@ function Rock() {
     	obj.speed = null;
     };
 }
+function ControlBank(){
+	
+}
--- _03_JSLoopsArraysAndStuff/js/_16_CardsFrequencies.js
+++ _03_JSLoopsArraysAndStuff/js/_16_CardsFrequencies.js
@@ -1,32 +1,26 @@
 function cardsPercentages(input) {
 	document.getElementById("result16").innerHTML = null;
-	var cards = ["A", "2", "3" , "4", "5", "6", "7", "8", "9", "10","J","Q", "K"];
+	var cards = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 	var percentages = [];
-
+	var pushed = 0;
 	var inp = input.value.split(" ");
 	for (var i = 0; i < cards.length; i++) {
-		percentages.push({card: cards[i], number: 0});
-	};
-	for (var i = 0; i < cards.length; i++) {
-		while (inp.toString().indexOf(cards[i]) != -1) {
-			inp.splice(inp.toString().indexOf(cards[i]),1);
-			percentages[i].number += 1;
-		};
+		percentages.push(0);
 	}
-
-	percentages.sort(compare);
+	for (var i = 0; i < inp.length; i++) {
+		for (var j = 0; j < cards.length; j++) {
+			if (inp[i].substr(0, cards[j].length) == cards[j]) {
+				percentages[j] += 1;
+			}
+		}
+	}
 	var multi = 100/inp.length;
 	for (var i = 0; i < percentages.length; i++) {
-		if(percentages[i].number > 0){
-			document.getElementById("result16").innerHTML = document.getElementById("result16").innerHTML + percentages[i].card + " : "+  percentages[i].number*multi +"<br>";
+		if(percentages[i] > 0){
+			document.getElementById("result16").innerHTML = document.getElementById("result16").innerHTML + cards[i] + " : "+  parseFloat(percentages[i]*multi).toFixed(2) +"%<br>";
 		}
-		else{
-			document.getElementById("result16").innerHTML = document.getElementById("result16").innerHTML + "kek";	
-		}
-	};
-
+	}
 }
-
 function compare(a, b){
 	if (a.number >  b.number){
 		return 1;
@@ -35,4 +29,4 @@ function compare(a, b){
 		return -1;
 	}
 	return 0;
-}
+} 
