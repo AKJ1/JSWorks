0a72b2cf1e074b1f4e8f76175af195b399464c34
AKJ1 <kellerr@abv.bg>
2014-07-22 18:38:15 +0300

    added 7 thru 9

---
+++ _04_FunctionsObjects/js/_09_.js
@@ -0,0 +1,12 @@
+function groupPersons(group, parameter){
+	if (parameter == ) {};
+}
+
+var getKeys = function() {
+
+    var keys = [];
+    for (var key in document) {
+        keys.push(key);
+    }
+    return keys;
+};
